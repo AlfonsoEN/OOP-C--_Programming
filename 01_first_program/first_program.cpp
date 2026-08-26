588f1a84074707e66f8c9be24d9eaffc88c650dc
Alfonso Nava <anava0880@floridapoly.edu>
2026-08-26 15:09:52 +0000

    meow

---
+++ 01_first_program/first_program.cpp
@@ -0,0 +1,33 @@
+/*
+
+    FIRST C++ PROGRAM (Not true, this is my second.)
+    08/19/2026
+
+*/
+
+#include <iostream>
+
+// Create the namespace
+
+namespace oop {
+
+    int courseID = 3337;
+
+}
+
+namespace intro {
+
+    int courseID = 2271;
+
+}
+
+using namespace std;
+
+
+int main(void) {
+
+    cout << "Hello COP3337" << endl;
+
+
+    return 0;   
+}
